4c369cf349164d25f65264687586e00236037377
popsy-pops <xcute.official@gmail.com>
2025-01-02 11:37:10 -0500

    done

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,10 @@
 export const config = {
     authSecret: process.env.AUTH_SECRET!,
 }
+
+export const cloudinaryConfig = {
+    uploadPreset: process.env.CLOUDINARY_UPLOAD_PRESET!,
+    preset: process.env.CLOUDINARY_API_KEY!,
+    apiSecret: process.env.CLOUDINARY_API_SECRET!,
+    url: process.env.CLOUDINARY_URL!
+}
